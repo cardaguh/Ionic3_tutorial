1f94fc3208bd871dcf985f03017249e1d3352472
cardaguh <cardaguh@gmail.com>
2017-10-20 14:14:34 -0500

    Tutorial de Ionic 3 para youtube

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AboutPage } from '../about/about'; 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  aboutPage = AboutPage;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  irAabout(){
+    this.navCtrl.push(this.aboutPage);
+  }
 }
